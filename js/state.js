bcd2a5d680e27309191cccc95d40a21d9dd992ef
pinto22 <51886210+Pinto2291@users.noreply.github.com>
2023-10-11 22:49:22 -0400

    mas css y javascript agregado, sitio ahora es responsive

--- js/state.js
+++ js/state.js
@@ -3,77 +3,80 @@ let stateRange = document.getElementById('state-range');
 let statusRandom = document.getElementById('status');
 let paragraph = document.getElementById('result-paragraph');
 let numbersContainer = document.getElementById('numbers-container');
+
 let reaction = document.getElementById('reaction');
 let memory = document.getElementById('memory');
 let verbal = document.getElementById('verbal');
 let visual = document.getElementById('visual');
-let buttonContinue = document.querySelector('.button');
 
-window.addEventListener('load', randomValues);
+/* percentajes */
 
-buttonContinue.addEventListener('click', randomValues);
+let reaction_percentaje = document.getElementById('reaction-percentaje');
+let memory_percentaje = document.getElementById('memory-percentaje');
+let verbal_percentaje = document.getElementById('verbal-percentaje');
+let visual_percentaje = document.getElementById('visual-percentaje');
+
+let button = document.querySelector('.button');
+
 
 function randomValues() {
-  let values = [];
-  for (let i = 1; i <= 4; i++) {
-    randomNumbers = Math.round(Math.random() * 100);
-    values.push(randomNumbers);
-  }
 
-  values.forEach((element, index) => {
-    switch (index) {
-      case 0:
-        reaction.innerText = element + ' / 100';
-        break;
-      case 1:
-        memory.innerText = element + ' / 100';
-        break;
-      case 2:
-        verbal.innerText = element + ' / 100';
-        break;
-      case 3:
-        visual.innerText = element + ' / 100';
-        break;
-      default:
-        'Error';
+    let values = [];
+
+    for(let i = 1; i <= 4; i++){
+        let randomNumbers = Math.round((Math.random()*100) + 1)
+        values.push(randomNumbers);
     }
-  });
 
-  const media = values.reduce((valuePrevious, currentValue) => {
-    return valuePrevious + currentValue;
-  });
+    values.forEach((element, index) => {
+        switch(index){
+            case 0:
+                reaction.innerText = element + ' / ';
+                reaction_percentaje.innerText = '100';
+                break;
+            case 1:
+                memory.innerText = element + ' / ';
+                memory_percentaje.innerText = '100';
+                break;
+            case 2:
+                verbal.innerText = element + ' / ';
+                verbal_percentaje.innerText = '100';
+                break;
+            case 3:
+                visual.innerText = element + ' / ';
+                visual_percentaje.innerText = '100';
+                break;
+            
+        }
+    })
 
-  let result = Math.round(media / 4);
+    const media = Math.round(values.reduce((prev, current) => {
+        return (prev + current);
+    })/values.length); 
 
-  if (result >= 75) {
-    resultContainer.classList.add('result-component');
-    numbersContainer.classList.add('numbers');
-    resultContainer.classList.remove('result-component-warning', 'result-component-danger');
-    numbersContainer.classList.remove('numbers-warning', 'numbers-danger')
-    stateRange.innerText = result;
-    statusRandom.innerText = 'Great';
-    paragraph.innerText = "You scored higher than 65% of the people who have taken these tests."
-  }
+    stateRange.innerText = media;
 
-  if (result <= 74 && result > 43) {
-    resultContainer.classList.add('result-component-warning');
-    numbersContainer.classList.add('numbers-warning');
-    resultContainer.classList.remove('result-component', 'result-component-danger');
-    numbersContainer.classList.remove('numbers', 'numbers-danger');
-    stateRange.innerText = result;
-    statusRandom.innerText = 'Warning';
-    paragraph.innerText = "You scored lower than 81% of the people who have taken these tests."
-
-  }
-
-  if (result <= 43) {
-    resultContainer.classList.add('result-component-danger');
-    numbersContainer.classList.add('numbers-danger');
-    resultContainer.classList.remove('result-component-warning', 'result-component');
-    numbersContainer.classList.remove('numbers', 'numbers-warning');
-    stateRange.innerText = result;
-    statusRandom.innerText = 'Danger';
-    paragraph.innerText = "You scored lower than 43% of the people who have taken these tests."
-  }
-
+    if(media >= 75) {
+        resultContainer.classList.add('result-component');
+        numbersContainer.classList.add('numbers');
+        resultContainer.classList.remove('result-component-warning', 'result-component-danger');
+        numbersContainer.classList.remove('numbes-warning', 'numbers-danger');
+        stateRange.innerText = media;
+        statusRandom.innerText = 'Great';
+        paragraph.innerText = 'You are a great person, keep going';
+    } else if (media <= 74 && media > 43){
+        resultContainer.classList.add('result-component-warning');
+        numbersContainer.classList.add('numbers-warning');
+        resultContainer.classList.remove('result-component-warning', 'result-component-danger');
+        numbersContainer.classList.remove('numbes-warning', 'numbers-danger');
+        stateRange.innerText = media;
+        statusRandom.innerText = 'Great';
+        paragraph.innerText = 'You are a great person, keep going';
+    }
 }
+
+
+
+button.addEventListener('click', () => {
+    randomValues();
+})
